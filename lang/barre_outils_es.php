db4f5b0d48d87493f5fe2d5e795ddf7a00cf1512
gaitan <gaitan@gmail.com>
2009-08-20 22:23:52 +0000

    add spanish translation file
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@31034 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/barre_outils_es.php
@@ -0,0 +1,92 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B Nouveautes 
+'barre_clean' => 'Limpiar el código de todas las etiquetas HTML',
+'barre_lorem_ipsum' => 'Insertar un párrafo ficticio',
+'barre_lorem_ipsum_3' => 'Insertar tres párrafos ficticios',
+'barre_liste_ul' => 'Convertir en lista',
+'barre_liste_ol' => 'Convertir en lista ordenada',
+'barre_indenter' => 'Convertir en sublista',
+'barre_desindenter' => 'Deshacer sublita',
+'barre_inserer_caracteres' => 'Insertar caracteres especiales',
+
+'editer'=>'Editar',
+'voir'=>'Ver',
+
+
+// B (depuis SPIP)
+'barre_a_accent_grave' => 'Insertar &Agrave;',
+'barre_aide' => 'Puedes enriquecer el formato de tu texto utilizando "atajos tipográficos"',
+'barre_e_accent_aigu' => 'Insertar &Eacute;',
+'barre_eo' => 'Insertar ligadura &oelig;',
+'barre_eo_maj' => 'Insertar  ligadura &OElig',
+'barre_euro' => 'Insertar símbolo &euro; ',
+'barre_gestion_cr_changercassemajuscules' => 'MAYÚSCULAS',
+'barre_gestion_cr_changercasseminuscules' => 'minúsculas',
+'barre_gras' => '{{negrita}}',
+'barre_guillemets' => 'Encerrar entre &laquo;comillas dobles&laquo;',
+'barre_guillemets_simples' => 'Place between &ldquo;single quotes&ldquo;',
+'barre_intertitre' => 'Convertir en {{{intertítulo}}}',
+'barre_italic' => '{cursiva}',
+'barre_lien' => 'Convertir en [hipevínculo->http://...]',
+'barre_lien_input' => 'Por favor ingresa el destino de tu hipervínculo (puede ser una URL de la forma http://www.misitio.com o simplmente el número de un artículo de este sitio).',
+'barre_note' => 'Convertir en [[Nota al pie]]',
+'barre_paragraphe' => 'Crear párrafo',
+'barre_quote' => '<quote>Citar</quote>',
+
+// B (depuis BTv2)
+'barre_langue' => 'Idioma abreviado',   //??
+'barre_miseenevidence' => 'Resaltar el [*texto*]',
+'barre_exposant' => 'Poner el texto en &lt;sup&gt;superíndice&lt;/sup&gt;',
+'barre_petitescapitales' => 'Poner el texto en &lt;sc&gt;mayúsculas pequeñas&lt;/sc&gt;',
+'barre_centrer' => '[|Centrar|] párrafo',
+'barre_alignerdroite' => '[/Alinear a derecha/]',
+'barre_encadrer' => '[(Párrafo con recuadro)]',
+'barre_e_accent_grave' => 'Intertar &Egrave;',
+'barre_ea' => 'Insertar ligadura &aelig;',
+'barre_ea_maj' => 'Insertar ligadura &AElig;',
+'barre_c_cedille_maj' => 'Insertar &Ccedil;',
+
+'barre_chercher' => 'Buscar y reemplazar',
+'barre_tableau' => 'Insertar/modificar una tabla (seleccionala primero)',
+'barre_glossaire' => '[?Referencia] (Wikipedia)',
+
+'barre_caracteres' => 'Caracteres especiales',
+'barre_lien_externe' => 'Enlace externo',
+'barre_bulle' => 'Globo de ayuda',
+'barre_galerie' => 'Ouvrir la galerie',
+'barre_gestion_caption' => 'Encabezado y resumen',
+
+'barre_formatages_speciaux' => 'Formateado especial',
+'barre_galerie' => 'Abrir la galería',
+'barre_gestion_colonne' => 'N&uacute;m de columnas',
+'barre_gestion_ligne' => 'N&uacute;m de l&iacute;neas',
+'barre_gestion_entete' => 'Encabezado',
+'barre_gestion_taille' => 'Tama&ntilde;o fijo',
+'barre_gestion_cr_changercasse' => 'Alternar',
+'barre_gestion_cr_changercassemajuscules' => 'Pasar a mayúsculas',
+'barre_gestion_cr_changercasseminuscules' => 'Pasar a minúsculas',
+'barre_gestion_cr_chercher' => 'Buscar',
+'barre_gestion_cr_remplacer' => 'Reemplazar',
+'barre_gestion_cr_casse' => 'Respetar mayúsculas y minúsculas',
+'barre_gestion_cr_tout' => 'Reemplazar todo',
+'barre_gestion_cr_entier' => 'Sólo palabras completas',
+'barre_preview' => 'Modo previsualización',
+'barre_stats' => 'Mostrar estadística del texto',
+
+'barre_code' => 'Mostrar como &lt;code&gt;código fuente&lt;/code&gt;',
+'barre_cadre' => 'Mostrar dentro de una  &lt;cadre&gt;caja de texto&lt;/cadre&gt;',
+'barre_poesie' => 'Mostrar como &lt;poesie&gt;Poesía&lt;/poesie&gt;',
+
+'barre_ancres' => 'Anclas',
+'barre_gestion_anc_caption' => 'Gestionar las anclas',
+'barre_gestion_anc_inserer' => 'Convertir a un ancla',
+'barre_gestion_anc_nom' => 'Nombre del ancla',
+'barre_gestion_anc_pointer' => 'Apuntar a un ancla',
+'barre_gestion_anc_cible' => 'Destino del ancla',
+'barre_gestion_anc_bulle' => 'Globo de ayuda de anclas',
+
+
+);
+?>
