f6e5a2eb7428e78f4d8cfc6cce6c567475c81c24
Matthieu Marcillaud <marcimat@rezo.net>
2009-09-05 14:58:55 +0000

    Grands changements dans porte plume (tester si tout continue de fonctionner !)
    * mise en fichier externe de la fonction de chargement de porte plume ce
    qui permet de la prendre en charge par le compacteur
    * permettre des insertions de barrs d'outils avec des classes javascript posés sur 
    les textarea : inserer_barre_forum, inserer_barre_edition, inserer_previsualisation
    * permettre de ne pas déposer la barre dans l'espace public avec la constante PORTE_PLUME_PUBLIC, définie à true par défaut (javascript et css présents dans le public)
    * donner une API pour créer des barres d'outils sur des textarea sélectionnés en javascript :
    .barre_outils($nom) et .barre_previsualisation().
    
    Exemple issu du core de porte plume :
    {{{
    $('.formulaire_spip textarea.inserer_barre_forum').barre_outils('forum');
    $('.formulaire_spip textarea.inserer_barre_edition').barre_outils('edition');
    $('.formulaire_spip textarea.inserer_previsualisation').barre_previsualisation();
    }}}
    
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@31353 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- porte_plume_pipelines.php
+++ porte_plume_pipelines.php
@@ -4,49 +4,35 @@
  * Licence GPL
  * Auteur Matthieu Marcillaud
  */
+define('PORTE_PLUME_PUBLIC', true);
 
-function porte_plume_insert_head($flux){
-	$lang = $GLOBALS['spip_lang'];
-	
+function porte_plume_insert_head_public($flux){
+	if (PORTE_PLUME_PUBLIC) {
+		$flux = porte_plume_inserer_head($flux, $GLOBALS['spip_lang']);
+	}
+	return $flux;
+}
+
+function porte_plume_insert_head_prive($flux){
+	$flux = porte_plume_inserer_head($flux, $GLOBALS['spip_lang']);
+	return $flux;
+}
+
+function porte_plume_inserer_head($flux, $lang){
 	$js = find_in_path('javascript/jquery.markitup_pour_spip.js');
 	$js_previsu = find_in_path('javascript/jquery.previsu_spip.js');
 	$js_settings = parametre_url(generer_url_public('porte_plume.js'), 'lang', $lang);
+	$js_start = parametre_url(generer_url_public('porte_plume_start.js'), 'lang', $lang);
 	$css = find_in_path('css/barre_outils.css');
 	$css_icones = generer_url_public('barre_outils_icones.css');
 
-	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n"
-		. "<link rel='stylesheet' type='text/css' media='all' href='$css_icones' />\n"
-		. "<script type='text/javascript' src='$js'></script>\n"
-		. "<script type='text/javascript' src='$js_previsu'></script>\n"
-		. "<script type='text/javascript' src='$js_settings'></script>\n";
-		
-	$preview = url_absolue(generer_url_public('porte_plume_preview'));
-	$tEditer = _T('barre_outils:editer');
-	$tVoir = _T('barre_outils:voir');
-	$flux .= <<<EOF
-		<script type="text/javascript">
-<!--
-jQuery(document).ready(function()	{
-	// ajoute les barres d'outils markitup
-	function barrebouilles(){
-		// si c'est un appel de previsu markitup, faut pas relancer
-		// on attrappe donc uniquement les textarea qui n'ont pas deja la classe markItUpEditor
-		jQuery('.formulaire_forum textarea[name=texte]:not(.markItUpEditor)').markItUp(barre_outils_forum,{lang:'$lang'});
-		jQuery('textarea.textarea_forum:not(.markItUpEditor)').markItUp(barre_outils_forum,{lang:'$lang'});
-		jQuery('.formulaire_spip textarea[name=texte]:not(.markItUpEditor)')
-			.markItUp(barre_outils_edition,{lang:'$lang'})
-			.previsu_spip({
-				previewParserPath:'$preview',
-				textEditer:'$tEditer',
-				textVoir:'$tVoir'});
-	}
-	barrebouilles();
-	onAjaxLoad(barrebouilles);
-
-});
--->		
-		</script>
-EOF;
+	$flux 
+		.= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n"
+		.  "<link rel='stylesheet' type='text/css' media='all' href='$css_icones' />\n"
+		.  "<script type='text/javascript' src='$js'></script>\n"
+		.  "<script type='text/javascript' src='$js_previsu'></script>\n"
+		.  "<script type='text/javascript' src='$js_settings'></script>\n"
+		.  "<script type='text/javascript' src='$js_start'></script>\n";
 
 	return $flux;
 }
