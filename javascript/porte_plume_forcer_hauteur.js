ea84f1d60576176a9949b9365d981c124e02c504
Cerdic <cedric@yterium.com>
2010-03-05 21:22:52 +0000

    redimensionner le textarea provoque un event size sur ie8, et donc une boucle infinie on se contente de redimensionner au chargement

--- javascript/porte_plume_forcer_hauteur.js
+++ javascript/porte_plume_forcer_hauteur.js
@@ -1,24 +1,24 @@
 function barre_forcer_hauteur () {
-	$(".markItUpEditor").each(function() {
-		var hauteur_min = $(this).height();
-		var hauteur_max = parseInt($(window).height()) - 200;
+	jQuery(".markItUpEditor").each(function() {
+		var hauteur_min = jQuery(this).height();
+		var hauteur_max = parseInt(jQuery(window).height()) - 200;
 		var hauteur = hauteur_min;
 
 		
-		var signes = $(this).val().length;
-		/* en gros: 400 signes donne 100 pixels de haut */
-		var hauteur_signes = Math.round(signes / 4) + 50;
-		if (hauteur_signes > hauteur_min && hauteur_signes < hauteur_max) hauteur = hauteur_signes;
-		else if (hauteur_signes > hauteur_max) hauteur = hauteur_max;
+		var signes = jQuery(this).val().length;
+		if (signes){
+			/* en gros: 400 signes donne 100 pixels de haut */
+			var hauteur_signes = Math.round(signes / 4) + 50;
+			if (hauteur_signes > hauteur_min && hauteur_signes < hauteur_max) 
+				hauteur = hauteur_signes;
+			else 
+				if (hauteur_signes > hauteur_max) 
+					hauteur = hauteur_max;
 		
-		$(this).height(hauteur);
-		
+			jQuery(this).height(hauteur);
+		}
+
 	});
 }
 
-$(document).ready(function(){
-	barre_forcer_hauteur();
-	$(window).bind("resize", function() {
-		barre_forcer_hauteur();
-	});
-});
+jQuery(barre_forcer_hauteur);
