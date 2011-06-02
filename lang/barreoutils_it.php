26847e38bb9f46f73632ea07602487387d822402
renato <renato@rezo.net>
2011-06-02 06:59:35 +0000

    langues (barreoutils)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@48389 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/barreoutils_it.php
+++ lang/barreoutils_it.php
@@ -8,31 +8,31 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'barre_a_accent_grave' => 'Inserisci una A con accento grave maiuscolo',
+	'barre_a_accent_grave' => 'Inserisci una A con accento grave maiuscolo', # MODIF
 	'barre_adresse' => 'Indirizzo',
 	'barre_aide' => 'Utilizza le scorciatoie tipografiche per arrichhire la tua impaginazione',
-	'barre_alignerdroite' => '[/Allinea a destra/] il paragrafo',
+	'barre_alignerdroite' => '[/Allinea a destra/] il paragrafo', # MODIF
 	'barre_ancres' => 'Gestione delle ancore',
 	'barre_barre' => 'Barra il testo',
 	'barre_bulle' => 'Tooltip',
-	'barre_c_cedille_maj' => 'Inserisci una C con cediglia maiuscola',
+	'barre_c_cedille_maj' => 'Inserisci una C con cediglia maiuscola', # MODIF
 	'barre_cadre' => 'Metti dentro una &lt;cadre&gt;zona di inserimento del testo&lt;/cadre&gt;',
-	'barre_caracteres' => 'Caratteri speciali',
+	'barre_caracteres' => 'Caratteri speciali', # MODIF
 	'barre_centrer' => '[|Centra|] il paragrafo',
 	'barre_chercher' => 'Cerca e sostituisci',
 	'barre_clean' => 'Elimina dal codice tutti i tag HTML',
 	'barre_code' => 'Impagina un &lt;code&gt;codice informatico&lt;/code&gt;',
-	'barre_desindenter' => 'Elimina l\'indentazione dela lista',
-	'barre_e_accent_aigu' => 'Inserisci una E con accento acuto maiuscola',
-	'barre_e_accent_grave' => 'Inserisci una E maiuscola con accento grave',
-	'barre_ea' => 'Inserisci una E nella A',
-	'barre_ea_maj' => 'Inserisci una E nella A maiuscola',
+	'barre_desindenter' => 'Elimina l\'indentazione dela lista', # MODIF
+	'barre_e_accent_aigu' => 'Inserisci una E con accento acuto maiuscola', # MODIF
+	'barre_e_accent_grave' => 'Inserisci una E maiuscola con accento grave', # MODIF
+	'barre_ea' => 'Inserisci una E nella A', # MODIF
+	'barre_ea_maj' => 'Inserisci una E nella A maiuscola', # MODIF
 	'barre_encadrer' => '[(Riquadra)] il paragrafo',
-	'barre_eo' => 'Inserisci una E con legatura alla O minuscola',
-	'barre_eo_maj' => 'Inserisci una E con legatura alla O maiuscola',
-	'barre_euro' => 'Inserisci il simbolo &euro;',
+	'barre_eo' => 'Inserisci una E con legatura alla O minuscola', # MODIF
+	'barre_eo_maj' => 'Inserisci una E con legatura alla O maiuscola', # MODIF
+	'barre_euro' => 'Inserisci il simbolo &euro;', # MODIF
 	'barre_exposant' => 'Metti il testo in &lt;sup&gt;esponente&lt;/sup&gt;',
-	'barre_formatages_speciaux' => 'Formattazioni speciali',
+	'barre_formatages_speciaux' => 'Formattazioni speciali', # MODIF
 	'barre_galerie' => 'Apri la galleria',
 	'barre_gestion_anc_bulle' => 'Tooltip ancora',
 	'barre_gestion_anc_caption' => 'Gestione delle ancore',
@@ -40,7 +40,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'barre_gestion_anc_inserer' => 'Trasforma in ancora',
 	'barre_gestion_anc_nom' => 'Nome dell\'ancora',
 	'barre_gestion_anc_pointer' => 'Punta verso un\'ancora',
-	'barre_gestion_caption' => 'Didascalia e riassunto',
+	'barre_gestion_caption' => 'Didascalia e riassunto', # MODIF
 	'barre_gestion_colonne' => 'Num colonne',
 	'barre_gestion_cr_casse' => 'Rispetta le maiuscole/minuscole',
 	'barre_gestion_cr_changercasse' => 'Cambia maiuscole/minuscole',
@@ -53,47 +53,47 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'barre_gestion_entete' => 'Intestazione',
 	'barre_gestion_ligne' => 'Num righe',
 	'barre_gestion_taille' => 'DImensione fissa',
-	'barre_glossaire' => 'Voce di [?glossario] (Wikipedia)',
+	'barre_glossaire' => 'Voce di [?glossario] (Wikipedia)', # MODIF
 	'barre_gras' => 'Converti in {{grassetto}}',
-	'barre_guillemets' => 'Racchiudi tra &laquo; virgolette &raquo;',
-	'barre_guillemets_simples' => 'Racchiudi tra &ldquo;virgolette di secondo livello&rdquo;',
+	'barre_guillemets' => 'Racchiudi tra &laquo; virgolette &raquo;', # MODIF
+	'barre_guillemets_simples' => 'Racchiudi tra &ldquo;virgolette di secondo livello&rdquo;', # MODIF
 	'barre_indenter' => 'Indenta la lista',
-	'barre_inserer_caracteres' => 'Inserisci dei caratteri speciali',
+	'barre_inserer_caracteres' => 'Inserisci dei caratteri speciali', # MODIF
 	'barre_intertitre' => 'Trasforma in {{{titolo}}}',
 	'barre_italic' => 'Converti in {corsivo}',
-	'barre_langue' => 'Acronimo lingua',
+	'barre_langue' => 'Acronimo lingua', # MODIF
 	'barre_lien' => 'Trasforma in [link ipertestuale->http://...]',
 	'barre_lien_externe' => 'Link esterno',
-	'barre_lien_input' => 'Indica l\'indirizzo del tuo link (puoi indicare un indirizo internet sotto forma di http://www.miosito.com, un indirizzo di posta elettronica, o semplicemente indicare il numero di un articolo di questo sito.',
-	'barre_liste_ol' => 'Converti in lista numerata',
+	'barre_lien_input' => 'Indica l\'indirizzo del tuo link (puoi indicare un indirizo internet sotto forma di http://www.miosito.com, un indirizzo di posta elettronica, o semplicemente indicare il numero di un articolo di questo sito.', # MODIF
+	'barre_liste_ol' => 'Converti in lista numerata', # MODIF
 	'barre_liste_ul' => 'Converti in lista',
-	'barre_lorem_ipsum' => 'Inserisci un paragrafo di prova (lorem ipsum)',
-	'barre_lorem_ipsum_3' => 'Inserisci 3 paragrafi di prova (lorem ipsum)',
-	'barre_miseenevidence' => 'Metti il testo in [*evidenza*]',
+	'barre_lorem_ipsum' => 'Inserisci un paragrafo di prova (lorem ipsum)', # MODIF
+	'barre_lorem_ipsum_3' => 'Inserisci 3 paragrafi di prova (lorem ipsum)', # MODIF
+	'barre_miseenevidence' => 'Metti il testo in [*evidenza*]', # MODIF
 	'barre_note' => 'Trasforma in [[nota a pi&egrave; pagina]]',
-	'barre_paragraphe' => 'Crea un paragrafo',
+	'barre_paragraphe' => 'Crea un paragrafo', # MODIF
 	'barre_petitescapitales' => 'metti il testo in &lt;sc&gt;maiuscoletto&lt;/sc&gt;',
-	'barre_poesie' => 'Impagina come una &lt;poesie&gt;poesia&lt;/poesie&gt;',
-	'barre_preview' => 'Modalit',
+	'barre_poesie' => 'Impagina come una &lt;poesie&gt;poesia&lt;/poesie&gt;', # MODIF
+	'barre_preview' => 'Modalit', # MODIF
 	'barre_quote' => '<quote>Cita un messaggio</quote>',
 	'barre_stats' => 'Mostra le statistiche del testo',
-	'barre_tableau' => 'Inserisci/modifica (selezionare prima) una tabella',
+	'barre_tableau' => 'Inserisci/modifica (selezionare prima) una tabella', # MODIF
 
 	// C
-	'config_info_enregistree' => 'Configurazione salvata',
+	'config_info_enregistree' => 'Configurazione salvata', # MODIF
 
 	// E
-	'editer' => 'Modifica',
+	'editer' => 'Modifica', # MODIF
 	'explication_barre_outils_public' => 'Gli script CSS e Javascript delle barre di testo
 	(estensione Porte Plume) vengono caricate nello spazio pubblico
 	e consentono di utilizzare queste barre sui moduli dei forum,
 	i pennarelli pubblici o per altri plugin, se le loro rispettive configurazioni
-	lo permettono.',
+	lo permettono.', # MODIF
 	'explication_barre_outils_public_2' => 'Puoi scegliere di non caricare
 	questi script al fine di allegerire il peso delle pagine pubbliche.
   Qualunque sia la configurazione dei forum, pennarelli o plugin,
   nessuna barra di testo di Prote Pplume potr&agrave; essere
-	inserita automaticamente nello spazio pubblico.',
+	inserita automaticamente nello spazio pubblico.', # MODIF
 
 	// I
 	'info_barre_outils_public' => 'Barra ti testo pubblica',
