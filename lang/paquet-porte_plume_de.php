a30470dd7ee637e549e4b18584c84d6069f74dc0
salvatore <salvatore@rezo.net>
2012-01-03 14:07:11 +0000

    langues (paquet-porte_plume)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@56318 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-porte_plume_de.php
+++ lang/paquet-porte_plume_de.php
@@ -1,21 +1,18 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-porte_plume
-// Langue: de
-// Date: 03-08-2011 19:45:21
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// P
+	// P
 	'porte_plume_description' => 'Der Federhalter ist eine erweiterbare Werkzeugleiste für SPIP auf Grundlage der Javascript-Bibiliothek [MarkItUp->http://markitup.jaysalvat.com/home/]
 
 	Die Icons stammen aus der Sammlung [FamFamFam->http://www.famfamfam.com/].',
 	'porte_plume_nom' => 'Federhalter',
-	'porte_plume_slogan' => 'Eine Meüleiste zum Verschönern der Texte',
+	'porte_plume_slogan' => 'Eine Meüleiste zum Verschönern der Texte'
 );
+
 ?>
