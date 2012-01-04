c4f686250f84a0555aaee3941218029c8adda66f
jrynik <jrynik@yahoo.co.uk>
2012-01-04 16:55:07 +0000

    langues (paquet-porte_plume)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@56517 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/paquet-porte_plume_sk.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'porte_plume_description' => 'Porte plume je rozšíriteľný panel s nástrojmi pre SPIP,
+	ktorý využíva javascriptovú knižnicu [MarkItUp->http://markitup.jaysalvat.com/home/]', # NEW
+	'porte_plume_nom' => 'Porte plume',
+	'porte_plume_slogan' => 'Panel s nástrojmi na to, aby ste dobre písali'
+);
+
+?>
