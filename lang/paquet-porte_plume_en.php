2603469e76636d684c3ceb726516d7962d6e78fa
salvatore <salvatore@rezo.net>
2012-01-14 10:34:39 +0000

    langues (paquet-porte_plume)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@57096 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-porte_plume_en.php
+++ lang/paquet-porte_plume_en.php
@@ -8,10 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'porte_plume_description' => 'The Quill is a SPIP extensible toolbar which uses the javascript library 
-	[MarkItUp->http://markitup.jaysalvat.com/home/]
-	
-	Icons are courtesy of the library [FamFamFam->http://www.famfamfam.com/]', # MODIF
+	'porte_plume_description' => 'The Quill is a SPIP extensible toolbar which uses the [MarkItUp->http://markitup.jaysalvat.com/home/] javascript library.',
 	'porte_plume_nom' => 'Quill',
 	'porte_plume_slogan' => 'A toolbar to enhance your texts'
 );
