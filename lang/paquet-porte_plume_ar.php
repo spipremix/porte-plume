985f49c0fd46cb45d2ec1101d00ab7d71a1c34d0
George Kandalaft <george@diwanalarab.com>
2012-02-13 09:48:43 +0000

    langues (paquet-porte_plume)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@58254 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-porte_plume_ar.php
+++ lang/paquet-porte_plume_ar.php
@@ -8,9 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// P
-	'porte_plume_description' => 'الريشة هي شريط أدوات موسع لنظام SPIP يستخدم مكتبة جافاسكريبت [MarkItUp->http://markitup.jaysalvat.com/home/]
-
-الأيقونات تأتي من مكتبة [FamFamFam->http://www.famfamfam.com/]', # MODIF
+	'porte_plume_description' => 'الريشة هي شريط أدوات موسع لنظام SPIP يستخدم مكتبة جافاسكريبت [MarkItUp-&gt;http://markitup.jaysalvat.com/home/]',
 	'porte_plume_nom' => 'الريشة',
 	'porte_plume_slogan' => 'شريط أدوات لتحسين الكتابة'
 );
