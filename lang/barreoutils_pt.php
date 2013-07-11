a5b81d5f7fc569324e11197da03407ad91eb0b3e
clanjlo <clanjlo@gmail.com>
2013-07-11 03:37:19 +0000

    langues (barreoutils)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@74233 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/barreoutils_pt.php
@@ -0,0 +1,112 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/barreoutils?lang_cible=pt
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'barre_a_accent_grave' => 'Inserir À',
+	'barre_adresse' => 'Endereço',
+	'barre_aide' => 'Usar os atalhos tipográficos para refinar o plano da página',
+	'barre_alignerdroite' => '[/Alinhar à direita/] o parágrafo',
+	'barre_ancres' => 'Gestião das âncoras',
+	'barre_barre' => 'Barrer le texte', # NEW
+	'barre_bulle' => 'Balão de ajuda',
+	'barre_c_cedille_maj' => 'Inserir Ç',
+	'barre_cadre' => 'Colocar numa &lt;cadre&gt;caixa de entrada de texto&lt;/cadre&gt;',
+	'barre_caracteres' => 'Caracteres especiais',
+	'barre_centrer' => '[|Centrar|] o parágrafo',
+	'barre_chercher' => 'Procurar & Substituir',
+	'barre_clean' => 'Limpar o código de todas as tags HTML',
+	'barre_code' => 'Editar um &lt;code&gt;código informático&lt;/code&gt;',
+	'barre_desindenter' => 'Désindenter une liste', # NEW
+	'barre_e_accent_aigu' => 'Inserir É',
+	'barre_e_accent_grave' => 'Inserir È',
+	'barre_ea' => 'Inserir æ',
+	'barre_ea_maj' => 'Inserir Æ',
+	'barre_encadrer' => '[(Enquadrar)] o parágrafo',
+	'barre_eo' => 'Inserir œ',
+	'barre_eo_maj' => 'Inserir Œ',
+	'barre_euro' => 'Inserir o símbolo €',
+	'barre_exposant' => 'Mettre le texte en &lt;sup&gt;exposant&lt;/sup&gt;', # NEW
+	'barre_formatages_speciaux' => 'Formatação especial',
+	'barre_galerie' => 'Abrir a galeria',
+	'barre_gestion_anc_bulle' => 'Balão de ajuda da âncora',
+	'barre_gestion_anc_caption' => 'Gestão das âncoras',
+	'barre_gestion_anc_cible' => 'Âncora-alvo',
+	'barre_gestion_anc_inserer' => 'Transformer en ancre', # NEW
+	'barre_gestion_anc_nom' => 'Nome da âncora',
+	'barre_gestion_anc_pointer' => 'Apontar para uma âncora',
+	'barre_gestion_caption' => 'Legenda e Resumo',
+	'barre_gestion_colonne' => 'Nb colonnes', # NEW
+	'barre_gestion_cr_casse' => 'Respecter la casse', # NEW
+	'barre_gestion_cr_changercasse' => 'Changer la casse', # NEW
+	'barre_gestion_cr_changercassemajuscules' => 'Passer en majuscules', # NEW
+	'barre_gestion_cr_changercasseminuscules' => 'Passer en minuscules', # NEW
+	'barre_gestion_cr_chercher' => 'Procurar',
+	'barre_gestion_cr_entier' => 'Apenas palavras completas',
+	'barre_gestion_cr_remplacer' => 'Substituir',
+	'barre_gestion_cr_tout' => 'Substituir tudo',
+	'barre_gestion_entete' => 'En-tête', # NEW
+	'barre_gestion_ligne' => 'Nb lignes', # NEW
+	'barre_gestion_taille' => 'Tamanho fixo',
+	'barre_glossaire' => 'Entrée du [?glossaire] (Wikipedia)', # NEW
+	'barre_gras' => 'Mettre en {{gras}}', # NEW
+	'barre_guillemets' => 'Entourer de « guillemets »', # NEW
+	'barre_guillemets_simples' => 'Entourer de “guillemets de second niveau”', # NEW
+	'barre_indenter' => 'Indenter une liste', # NEW
+	'barre_inserer_cadre' => 'Inserir um código pré-formatado (cadre)',
+	'barre_inserer_caracteres' => 'Insérer des caractères spécifiques', # NEW
+	'barre_inserer_code' => 'Insérer un code informatique (code)', # NEW
+	'barre_intertitre' => 'Transformer en {{{intertitre}}}', # NEW
+	'barre_italic' => 'Mettre en {italique}', # NEW
+	'barre_langue' => 'Idioma abreviado',
+	'barre_lien' => 'Transformer en [lien hypertexte->http://...]', # NEW
+	'barre_lien_externe' => 'Ligação externa',
+	'barre_lien_input' => 'Veuillez indiquer l\'adresse de votre lien (vous pouvez indiquer une adresse Internet sous la forme http://www.monsite.com, une adresse courriel, ou simplement indiquer le numéro d\'un article de ce site.', # NEW
+	'barre_liste_ol' => 'Colocar em lista numerada',
+	'barre_liste_ul' => 'Colocar em lista',
+	'barre_lorem_ipsum' => 'Insérer un paragraphe factice', # NEW
+	'barre_lorem_ipsum_3' => 'Insérer trois paragraphes factices', # NEW
+	'barre_miseenevidence' => '[*Realçar*] o texto',
+	'barre_note' => 'Transformer en [[Note de bas de page]]', # NEW
+	'barre_paragraphe' => 'Créer un paragraphe', # NEW
+	'barre_petitescapitales' => 'Mettre le texte en &lt;sc&gt;petites capitales&lt;/sc&gt;', # NEW
+	'barre_poesie' => 'Mettre en forme comme une &lt;poesie&gt;poésie&lt;/poesie&gt;', # NEW
+	'barre_preview' => 'Modo pré-visualização',
+	'barre_quote' => '<quote>Citar uma mensagem</quote>',
+	'barre_stats' => 'Afficher les statistiques du texte', # NEW
+	'barre_tableau' => 'Insérer/modifier (le sélectionner avant) un tableau', # NEW
+
+	// C
+	'config_info_enregistree' => 'Configuração guardada',
+
+	// E
+	'editer' => 'Editar',
+	'explication_barre_outils_public' => 'Les scripts CSS et Javascript des barre d\'outils
+	(extension Porte Plume) sont chargés sur l\'espace public
+	et permettent d\'utiliser ces barres sur les formulaires de forums,
+	les crayons publics ou pour d\'autres plugins, si leurs configurations
+	respectives le permettent.', # NEW
+	'explication_barre_outils_public_2' => 'Vous pouvez choisir de ne pas charger
+	ces scripts afin d\'alléger le poids des pages publiques.
+	Dès lors quelle que soit la configuration des forums, crayons ou plugin,
+	aucune barre d\'outils du Porte Plume ne pourra être
+	présente automatiquement sur l\'espace public.', # NEW
+
+	// I
+	'info_barre_outils_public' => 'Barra de ferramentas pública',
+	'info_porte_plume_titre' => 'Configurer les barres d\'outils', # NEW
+
+	// L
+	'label_barre_outils_public_non' => 'Ne pas charger les scripts de barre d\'outils sur l\'espace public', # NEW
+	'label_barre_outils_public_oui' => 'Charger les scripts de barre d\'outils sur l\'espace public', # NEW
+
+	// V
+	'voir' => 'Visualizar'
+);
+
+?>
