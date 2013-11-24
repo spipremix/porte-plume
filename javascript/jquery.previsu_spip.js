b7afd3797be49d75ae8eaf94873688de03c41182
Cerdic <cedric@yterium.com>
2013-11-24 15:33:28 +0000

    Nouvelle fonctionnalité : edition en mode plein écran split-screen avec l'édition de texte à gauche et la prévisu live à droite.
    Il faudrait synchroniser les scrolls entre les 2 vues, mais c'est non trivial, car à cause des notes de bas de page on peut être à 100% du scroll en édition et que le texte modifié corresponde à 50% du scroll en prévisu.
    Sans doute faudrait-il repérer la portion de texte qui change entre 2 mises à jour de la prévisu et positionner le scroll dessus si besoin ?
    
    A priori OK sur FF et Safari, a tester plus largement sur les autres navigateurs.
    
    On renomme le dossier css/images en css/img par convention
    
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@78782 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- javascript/jquery.previsu_spip.js
+++ javascript/jquery.previsu_spip.js
@@ -6,7 +6,8 @@
 			previewParserPath:	'' ,
 			previewParserVar:	'data',
 			textEditer:	'Editer',
-			textVoir:	'Voir'
+			textVoir:	'Voir',
+			textFullScreen: 'Plein écran'
 		};
 		$.extend(options, settings);
 
@@ -21,13 +22,49 @@
 				$$.addClass("pp_previsualisation");
 				tabs = $('<div class="markItUpTabs"></div>').prependTo($$.parent());
 				$(tabs).append(
+					'<a href="#fullscreen" class="fullscreen">' + options.textFullScreen + '</a>' +
 					'<a href="#previsuVoir" class="previsuVoir">' + options.textVoir + '</a>' +
 					'<a href="#previsuEditer" class="previsuEditer on">' + options.textEditer + '</a>'
 				);
 				
 				preview = $('<div class="markItUpPreview"></div>').insertAfter(tabs);
 				preview.hide();
-				
+
+				var is_full_screen = false;
+				$('.fullscreen').click(function(){
+					mark = $(this).parent().parent();
+					mark.toggleClass('fullscreen');
+					objet = mark.parents('.formulaire_spip')[0].className.match(/formulaire_editer_(\w+)/);
+					champ = mark.parents('li')[0].className.match(/editer_(\w+)/);
+					if (mark.is('.fullscreen')){
+						is_full_screen = true;
+						if (!mark.is('.livepreview')){
+							var original_texte="";
+							function refresh_preview(){
+								var texte = $(mark).find('textarea.pp_previsualisation').val();
+								if (original_texte == texte){
+									return;
+								}
+								renderPreview($(mark).find('.markItUpPreview').addClass('ajaxLoad'),
+										texte,
+										champ[1].toUpperCase(),
+										(objet ? objet[1] : ''));
+								original_texte = texte;
+							}
+							var timerPreview=null;
+							mark.addClass('.livepreview').find('.markItUpEditor').bind('keyup click change focus refreshpreview',function(){
+								if (is_full_screen){
+									if (timerPreview) clearTimeout(timerPreview);
+									timerPreview = setTimeout(refresh_preview,500);
+								}
+							})
+						}
+						mark.find('.markItUpEditor').trigger('refreshpreview');
+					}
+					else
+						is_full_screen = false;
+				});
+
 				$('.previsuVoir').click(function(){
 					mark = $(this).parent().parent();
 					objet = mark.parents('.formulaire_spip')[0].className.match(/formulaire_editer_(\w+)/);
@@ -42,17 +79,10 @@
 					$(mark).find('.markItUpEditor').hide();
 					$(mark).find('.markItUpFooter').hide();
 					$(this).addClass('on').next().removeClass('on');
-					$(mark).find('.markItUpPreview').show()
-						.addClass('ajaxLoad')
-						.html(renderPreview(
+					renderPreview($(mark).find('.markItUpPreview').show().addClass('ajaxLoad'),
 							$(mark).find('textarea.pp_previsualisation').val(),
 							champ[1].toUpperCase(),
-							(objet ? objet[1] : ''))
-						)
-						.removeClass('ajaxLoad');
-					
-					//ouvre un nouvel onglet lorsqu'on clique sur un lien dans la prévisualisation
-					$(".markItUpPreview a").attr("target","blank");
+							(objet ? objet[1] : ''));
 
 					return false;
 				});
@@ -68,8 +98,7 @@
 			}
 
 
-			function renderPreview(val, champ, objet) {
-				var phtml;
+			function renderPreview(node, val, champ, objet) {
 				if (options.previewParserPath !== '') {
 					$.ajax( {
 						type: 'POST',
@@ -79,11 +108,12 @@
 							+'&objet='+objet
 							+'&' + options.previewParserVar+'='+encodeURIComponent(val),
 						success: function(data) {
-							phtml = data; 
+							node.html(data).removeClass('ajaxLoad');
+							//ouvre un nouvel onglet lorsqu'on clique sur un lien dans la prévisualisation
+							$("a",node).attr("target","blank");
 						}
 					} );
 				}
-				return phtml;
 			}
 	
 			init();
