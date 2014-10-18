03e00d418a640d666fd23c1df71fcf0ebee422e0
telopa <telopa@gmail.com>
2014-10-18 03:38:19 +0000

    [Salvatore] [source:_core_/plugins/porte_plume/lang/ barreoutils] Export depuis http://trad.spip.net de la langue pt_br
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/porte_plume@85356 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/barreoutils_pt_br.php
@@ -0,0 +1,76 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/barreoutils?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'barre_a_accent_grave' => 'Inserir À',
+	'barre_adresse' => 'Endereço',
+	'barre_aide' => 'Use atalhos de digitação para melhorar o layout',
+	'barre_alignerdroite' => 'Alinhar parágrafo [/à direita/]',
+	'barre_ancres' => 'Gestão de âncoras',
+	'barre_barre' => 'Tachar o texto',
+	'barre_bulle' => 'Bolha de ajuda',
+	'barre_c_cedille_maj' => 'Inserir Ç',
+	'barre_caracteres' => 'Caracteres especiais',
+	'barre_chercher' => 'Localizar Substituir',
+	'barre_clean' => 'Limpar o código de toas as HTML',
+	'barre_desindenter' => 'Desindentar uma lista',
+	'barre_e_accent_aigu' => 'Inserir É',
+	'barre_e_accent_grave' => 'Inserir È',
+	'barre_ea' => 'Inserir æ',
+	'barre_encadrer' => '[(Enquadrar)] o parágrafo',
+	'barre_formatages_speciaux' => 'Formatação especial',
+	'barre_galerie' => 'Abrir a galeria',
+	'barre_gestion_anc_bulle' => 'Bolha de ajuda da âncora',
+	'barre_gestion_anc_caption' => 'Gestão de âncoras',
+	'barre_gestion_anc_cible' => 'alvo da âncora',
+	'barre_gestion_anc_inserer' => 'Transformar em âncora',
+	'barre_gestion_anc_nom' => 'Nome da âncora',
+	'barre_gestion_anc_pointer' => 'Ponteiro para uma âncora',
+	'barre_gestion_caption' => 'Legenda e Resumo',
+	'barre_gestion_cr_chercher' => 'Buscar',
+	'barre_gestion_cr_tout' => 'Substituir tudo',
+	'barre_glossaire' => 'Referência [?glossaire] (Wikipedia)',
+	'barre_gras' => 'Pôr em {{negrito}}',
+	'barre_indenter' => 'Indentar uma lista',
+	'barre_inserer_cadre' => 'Insirir código pré-formatado (framework)',
+	'barre_inserer_caracteres' => 'Inserir caracteres especiais',
+	'barre_inserer_code' => 'Inserir código de informática (code)',
+	'barre_intertitre' => 'Transformar em {{{intertítulo}}}',
+	'barre_italic' => 'Pôr em {itálico}',
+	'barre_lien' => 'Transformar em [link ->http://...]',
+	'barre_lien_externe' => 'Link externo',
+	'barre_liste_ol' => 'Pôr em lista numerada',
+	'barre_liste_ul' => 'Pôr em lista',
+	'barre_lorem_ipsum' => 'Insira um parágrafo nonono',
+	'barre_lorem_ipsum_3' => 'Inserir três parágrafos nonono',
+	'barre_note' => 'Transformar em [[Nota de rodapé]]',
+	'barre_quote' => '<quote>Fazer uma citação</quote>',
+	'barre_tableau' => 'Inserir/modificar uma tabela (selecione antes)',
+
+	// C
+	'config_info_enregistree' => 'Configuração salva',
+
+	// E
+	'editer' => 'Editar',
+	'explication_barre_outils_public' => 'Os scripts CSS e Javascript das barras de ferramentas (extensão Porte Plume) são carregadas no espaço público e permitem a utilização dessas barras nos formulários de forums, crayons públicos ou por outros plugins, se suas respectivas configurações o permitirem.',
+	'explication_barre_outils_public_2' => 'Você pode optar por não carregar esses scripts para aliviar a carga de páginas públicas. Portanto, independentemente da configuração dos fóruns, crayons ou plugins, nenhuma barra de ferramentas do Porte Plume será disponibilizado automaticamente no espaço público.',
+
+	// I
+	'info_barre_outils_public' => 'Barra de ferramentas pública',
+	'info_porte_plume_titre' => 'Configurar as barras de ferramentas',
+
+	// L
+	'label_barre_outils_public_non' => 'Não carregar os scripts da barra de ferramentas no espaço público',
+	'label_barre_outils_public_oui' => 'Carregar os scripts da barra de ferramentas no espaço público',
+
+	// V
+	'voir' => 'Ver'
+);
+
+?>
