92fd8a150e9dd4b5752e7d8f2b56a52d3c189792
Matthieu Marcillaud <marcimat@rezo.net>
2017-07-27 08:48:28 +0000

    Compat PHP7 des tests.

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -2,9 +2,8 @@
 require_once('lanceur_spip.php');
 
 class AllTests_barre_outil_markitup extends SpipTestSuite {
-	function AllTests_barre_outil_markitup() {
-		$this->SpipTestSuite('Barre MarkitUp');
+	function __construct() {
+		parent::__construct('Barre MarkitUp');
 		$this->addDir(__FILE__);
-		#$this->addFile(find_in_path('tests/barre_outil_markitup.php'));
 	}
 }
