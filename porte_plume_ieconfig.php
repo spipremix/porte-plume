0834583ea3fc6a75d1a7398d51dee0c72718b2e5
Cerdic <cedric@yterium.com>
2018-09-27 14:06:32 +0000

    Fix https://core.spip.net/issues/4115 : eviter une collision de nommage dans ieconfig (est-ce que ca a sa place ici, vraiment?)

--- porte_plume_ieconfig.php
+++ porte_plume_ieconfig.php
@@ -23,9 +23,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  *     Déclaration des sauvegardes complétées
  **/
 function porte_plume_ieconfig_metas($table) {
-	$table['porte_plume']['titre'] = _T('barreoutils:info_barre_outils_public');
-	$table['porte_plume']['icone'] = 'porte-plume-16.png';
-	$table['porte_plume']['metas_brutes'] = 'barre_outils_public';
+	$table['porte_plume_meta']['titre'] = _T('barreoutils:info_barre_outils_public');
+	$table['porte_plume_meta']['icone'] = 'porte-plume-16.png';
+	$table['porte_plume_meta']['metas_brutes'] = 'barre_outils_public';
 
 	return $table;
 }
